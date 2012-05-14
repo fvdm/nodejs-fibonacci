050ce5de03d6b5b6bd17d4b172ce7e5074092add
Franklin <fr@nkl.in>
2012-05-15 00:09:12 +0200

    Converted to module for endless fun

--- fibonacci.js
+++ fibonacci.js
@@ -19,40 +19,74 @@ It returns an object with these elements:
 
 Usage:
 var fibonacci = require('fibonacci');
-var bigNumber = fibonacci( 5000 );
+var bigNumber = fibonacci.iterate( 3000 );
 console.log( bigNumber );
+
+Get all numbers:
+WARNING: THIS CONTINUES FOREVER !!!
+         Kill with fibonacci.kill();
+
+fibonacci.on( 'result', function( number ) {
+	console.log( number.iterations +') '+ number.number );
+	if( number.ms > 10000 ) {
+		fibonacci.kill();
+	}
+});
+fibonacci.iterate();
 */
 
-var bignum = require('bignum');
+var	bignum = require('bignum'),
+	EventEmitter = require('events').EventEmitter;
 
-module.exports = function( limit ) {
+var app = new EventEmitter();
+
+app.iterate = function( limit ) {
 	var	next = bignum(1),
 		last = bignum(0),
 		loop = bignum(0),
-		start = new Date().getTime(),
-		ok = true;
+		start = new Date().getTime();
 	
-	while( ok ) {
+	app.doWhile = true;
+	
+	while( app.doWhile ) {
 		loop = loop.add(1);
 		cur = last;
 		last = next;
 		next = next.add(cur);
 		
-		// found it
-		if( loop == limit ) {
-			ok = false;
-			return {
-				number:		next.toString(),
-				length:		next.toString().length,
-				iterations:	loop.toString(),
-				ms:			new Date().getTime() - start
-			}
+		var result = {
+			number:		next.toString(),
+			length:		next.toString().length,
+			iterations:	loop.toString(),
+			ms:			new Date().getTime() - start
+		}
+		
+		app.emit( 'result', result );
+		
+		// found the one
+		if( limit !== undefined && loop == limit ) {
+			app.doWhile = false;
+			return result;
 		}
 		
 		// catch infinity
 		if( next == 'Infinity' ) {
-			ok = false;
+			app.doWhile = false;
+			app.emit( 'stop', {
+				reason:			'infinity',
+				max_limit:		Number.MAX_LIMIT,
+				last_result:	result,
+				iterations:		loop.toString(),
+				intended:		limit ? limit : false
+			});
 			break;
 		}
 	}
 }
+
+app.kill = function() {
+	app.doWhile = false;
+}
+
+// ready
+module.exports = app;

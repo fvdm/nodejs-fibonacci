aa3b84898ff948e64904c1e1a45d389d6b23fc0f
Franklin van de Meent <fr@nkl.in>
2016-07-01 22:25:20 +0200

    Test(package): Add test.sh runner and dev deps
    - coveralls
    - istanbul

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+result=0
+
+eslint *.js || result=1
+istanbul cover test.js || result=1
+
+if [ $TRAVIS == "true" ]; then
+  [ $result == 0 ] && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v || result=1
+else
+  node test.js || result=1
+fi
+
+exit $result
+

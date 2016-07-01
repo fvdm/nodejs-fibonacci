7e92b1113a062081c7a1a49d016f5059ca1d74bd
Franklin van de Meent <fr@nkl.in>
2016-07-01 22:45:01 +0200

    Test(runner): Fixed if condition incompatibility

--- test.sh
+++ test.sh
@@ -4,9 +4,8 @@ result=0
 eslint *.js || result=1
 istanbul cover test.js || result=1
 
-if [ $TRAVIS == "true" ]; then
-  [ $result == 0 ] && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v || result=1
+if [ "$TRAVIS" == "true" ] && [ "$result" == "0" ]; then
+  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v || result=1
 fi
 
 exit $result
-

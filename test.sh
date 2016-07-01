eeabb966667d1b49ccf5c5b631c6bed1db9ecd0d
Franklin van de Meent <fr@nkl.in>
2016-07-01 22:39:58 +0200

    Test(runner): Istanbul already runs test.js

--- test.sh
+++ test.sh
@@ -6,8 +6,6 @@ istanbul cover test.js || result=1
 
 if [ $TRAVIS == "true" ]; then
   [ $result == 0 ] && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js -v || result=1
-else
-  node test.js || result=1
 fi
 
 exit $result
